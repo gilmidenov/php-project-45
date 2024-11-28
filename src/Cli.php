9ec9d1cf746954ee3c596de2d451fe39210c60ec
Адильжан Гилмиденов <a.gilmidenov@AD.DG>
2024-11-29 02:50:53 +0500

    brain-games step 3 final

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function run()
+function welcome()
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
